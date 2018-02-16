f0880296fbe5910e401610303f27aa0749f1a174
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-16 19:08:09 +0330

    modified gitignore

--- smart-contract-kickstarter/.next/dist/pages/_document.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = require('next\\dist\\server\\document.js');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlc1xcbmV4dFxcZGlzdFxccGFnZXNcXF9kb2N1bWVudC5qcyJdLCJuYW1lcyI6WyJtb2R1bGUiLCJleHBvcnRzIiwicmVxdWlyZSJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoiX2RvY3VtZW50LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkQ6L1Byb2plY3RzL1NvbGlkaXR5X1Byb2plY3RzL2tpY2tzdGFydGVyL3NtYXJ0LWNvbnRyYWN0LWtpY2tzdGFydGVyIn0=
