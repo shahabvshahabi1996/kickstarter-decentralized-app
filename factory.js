157365d2f169164d1d28ba9bcb899cf24f9d93fa
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-17 11:03:30 +0330

    fixing bugs

--- factory.js
+++ factory.js
@@ -1,9 +1,10 @@
 import web3 from './web3';
 import CampaignFactory from "./build/CampaignFactory.json";
+import {address} from './build/address.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xbdD499AD478Db30E4f6b6FF01F923F47e01cfd46'
+    address
 );
 
 export default instance;
