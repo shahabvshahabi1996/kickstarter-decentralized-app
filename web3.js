45b91c6adf75abbb85e0688403081fca682c6519
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-18 03:07:02 +0330

    fixing bugs of provider

--- web3.js
+++ web3.js
@@ -5,7 +5,7 @@ if(typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
     web3 = new Web3(window.web3.currentProvider);
 }
 else{
-    const provider = Web3.providers.HttpProvider(
+    const provider = new Web3.providers.HttpProvider(
         'https://rinkeby.infura.io/YfN9MatBIrqHOPqfZz5l'
     );
 
