0719eda3862af1380db326a108884527ccb47ced
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-19 00:58:25 +0330

    added simpleCards Component to Project

---
+++ pages/components/SimpleCards.js
@@ -0,0 +1,48 @@
+import React , {Component} from 'react';
+import { Button, Card, Grid , Divider , Container , Image , Icon , Popup } from 'semantic-ui-react';
+
+export default class SimpleCards extends Component{
+    render(){
+        return(
+
+        <Grid columns={3}>
+        {this.props.datas.map(data => {
+            return(
+            <Grid.Row style={{justifyContent : 'center',alignItems : 'center'}}>
+                <Grid.Column width={4}>
+                    <Image  size='small' src='https://placeholdit.co//i/500x300?bg=eeeeee' />
+                </Grid.Column>
+                <Grid.Column width={10}>
+                    <Card fluid style={{boxShadow : 'none'}}>
+                        <Card.Content>
+                            <Card.Header>
+                            {data.name}
+                            </Card.Header>
+                            <Card.Meta>
+                            {data.budget} funded
+                            </Card.Meta>
+                            <Card.Description>
+                            {data.description}
+                            </Card.Description>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+                <Grid.Column textAlign='center' width={2}>
+                    <Popup
+                    trigger={<Icon name='heart outline' color='#eee' size='large' />}
+                    content='Archive it'
+                    position='top center'
+                    />
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    <Grid.Row>
+                        <Divider fitted />
+                    </Grid.Row>    
+                </Grid.Column>
+            </Grid.Row>
+            )
+        })}
+        </Grid>
+        )
+    }
+}
