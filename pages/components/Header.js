1f24bc3cd4582a0acb5459bb229be42d71d3967e
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-19 14:00:52 +0330

    added header.js file

---
+++ pages/components/Header.js
@@ -0,0 +1,9 @@
+import React , {Component} from 'react';
+
+export default class extends Component{
+    render(){
+        return(
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+        )
+    }
+}
