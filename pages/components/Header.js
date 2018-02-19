b8fd7f9011af155ff113343e00ca9539b8176525
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-19 23:16:51 +0330

    TopCampaigns.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,7 +3,9 @@ import React , {Component} from 'react';
 export default class extends Component{
     render(){
         return(
+            <div>
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+            </div>
         )
     }
 }
