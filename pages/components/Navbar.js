9a2d733f26b6680b2cc2e6dad2611b2d83521489
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-19 23:18:37 +0330

    refactoring navbar styles

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,6 +1,9 @@
 import React , {Component} from "react";
-import { Button, Card, Grid , Divider , Menu , Container , Image , Icon , Popup , Label } from 'semantic-ui-react';
+import { Button, Card, Grid , Divider , Menu , Input , Container , Image , Icon , Popup , Label } from 'semantic-ui-react';
 import Header from './Header';
+
+// import '../styles/App.css';
+
 export default class Navbar extends Component{
     state = {}
 
@@ -10,28 +13,31 @@ export default class Navbar extends Component{
       const { activeItem } = this.state
   
       return (
-        <div>
-          <Header/>
-          <Menu style={{boxShadow : 'none',borderRadius : 0,margin : 0,padding : '.5%'}}>
+          <Menu borderless stackable style={{padding : '5px',borderRadius : 0,marginTop:0,borderColor : '#eee',boxShadow: '0px 5px 8px 0px rgba(0,0,0,0.15)',borderWidth : 1,borderStyle : 'solid',padding : '0px'}}>
+            <Header/>
+            <Menu.Item>
+              <h2>KickStarter</h2>
+            </Menu.Item>
             <Menu.Item name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick}>
-              Explore
+            <h4>Explore</h4>
             </Menu.Item>
-    
             <Menu.Item name='submit' active={activeItem === 'submit'} onClick={this.handleItemClick}>
-              Start A Project
+              <h4>Start a project</h4>
             </Menu.Item>
-    
-            <Menu.Menu style={{border : '0'}} position='right'>
-              <Menu.Item name='signup' active={activeItem === 'signup'} onClick={this.handleItemClick}>
-              Sign Up
+
+            <Menu.Menu position='right'>
+              <Menu.Item>
+              <Popup
+                trigger={<Input icon='search' placeholder='Search...' />}
+                header='Campaign Search'
+                content='Look for your dreamy project,and make it alive!'
+                on='focus'/>
               </Menu.Item>
-    
               <Menu.Item name='help' active={activeItem === 'help'} onClick={this.handleItemClick}>
-              Search
+                <h4>Sign In</h4>
               </Menu.Item>
             </Menu.Menu>
           </Menu>
-        </div>
       )
     }
 }
