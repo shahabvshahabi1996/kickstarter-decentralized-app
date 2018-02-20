07b5e8b33fe03dbceca9ae3c2591333f7cb1384d
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-21 03:22:42 +0330

    added recomended and Ourblog part to the index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React , {Component} from "react";
-import { Button, Grid , Divider , Container , Tab , Image , Icon , Popup , Label } from 'semantic-ui-react';
+import { Button, Grid , Divider , Container , Advertisement , Tab , Image , Icon , Popup , Label } from 'semantic-ui-react';
 import SimpleCard from './components/SimpleCards';
 import Navbar from './components/Navbar';
 import TopCampaigns from './components/TopCampaigns';
 import Cards from './components/Cards'; 
+import BlogCards from './components/BlogCards'; 
 
 import factory from '../factory';
 
@@ -22,32 +23,7 @@ const datas = [
         name : "Something new to Another world bag for Minimalists",
         budget : "25",
         description : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
-    },
-    {
-        name : "Somthing new to this world bag for Minimalists",
-        budget : "2",
-        description : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
-    },
-    {
-        name : "Something new to That World bag for Minimalists",
-        budget : "32",
-        description : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
-    },
-    {
-        name : "Something new to Another world bag for Minimalists",
-        budget : "250",
-        description : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
-    },
-    {
-        name : "Somthing new to this world bag for Minimalists",
-        budget : "10",
-        description : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
-    },
-    {
-        name : "Something new to Another world bag for Minimalists",
-        budget : "25",
-        description : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
-    },
+    }
 ]
 
 
@@ -61,17 +37,33 @@ export default class CampaignIndex extends Component {
     }
     render(){
         return(
-            <div>    
+            <div style={{backgroundColor:'rgba(65,109,234,1)'}}>    
                 <Navbar/>
-                <Container style={{padding : '10px'}}>
-                    <h1>Top Campaigns<span><Button floated="right" icon labelPosition='right'>View All <Icon name='right arrow' /> </Button></span></h1>
-                    {/* <Divider /> */}
-                    {/* <TopCampaigns datas={datas} /> */}
-                    <Cards datas={datas}/>
-                </Container>
-                <div style={{borderColor : '#eee',borderStyle:'solid',borderWidth : 1,marginTop : '10px',marginBottom : '10px',padding : '15px'}}>
-                    <Container>
-                        <h1>Recomended For You</h1>
+                <div>
+                    <Container style={{padding : '15px',margin: '10px'}}>
+                        <h1 style={{color : '#252525'}}><span style={{backgroundColor : '#fff',borderRadius : 1.5,padding : '2px',paddingLeft : '5px',paddingRight : '5px'}}>Top Campaigns</span><span><Button icon floated="right" style={{borderRadius : 2,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}} labelPosition='right'>View All <Icon name='right arrow' /> </Button></span></h1>
+                        <Cards datas={datas}/>
+                    </Container>
+                </div>
+                <div style={{marginBottom : '10px',borderColor : '#eee',borderStyle:'solid',borderWidth : 1,marginTop : '10px',marginBottom : '0',padding : '15px',backgroundColor : '#fff'}}>
+                    <Container style={{margin : '10px'}}>
+                        <h1>
+                            <span style={{backgroundColor:'rgba(65,109,234,1)',padding : '2px' , paddingLeft : '5px' , paddingRight : '5px',color : '#fff'}}>
+                            Recomended
+                            </span>
+                            <span>
+                                <Button icon floated="right" style={{borderRadius : 2,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}} labelPosition='right'>
+                                View All <Icon name='right arrow' />
+                                </Button>
+                            </span>
+                        </h1>
+                        <Cards datas={datas}/>
+                    </Container>
+                </div>
+                <div>
+                    <Container style={{padding : '15px',margin: '10px'}}>
+                        <h1 style={{color : '#252525'}}><span style={{backgroundColor : '#fff',borderRadius : 1.5,padding : '2px',paddingLeft : '5px',paddingRight : '5px'}}>Our Blog</span><span><Button icon floated="right" style={{borderRadius : 2,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}} labelPosition='right'>View All <Icon name='right arrow' /> </Button></span></h1>
+                        <BlogCards/>                        
                     </Container>
                 </div>
             </div>
