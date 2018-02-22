73867ed30ca568a8b1a045f20d254ca9037b52df
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-23 01:06:07 +0330

    added new category list

--- pages/components/Category.js
+++ pages/components/Category.js
@@ -7,17 +7,14 @@ export default class Category extends Component{
         return(
             <Dropdown text='Design & Tech' floating>
                 <Dropdown.Menu fluid>
-                    <Dropdown.Item text='New' />
-                    <Dropdown.Item text='Open...' description='ctrl + o' />
-                    <Dropdown.Item text='Save as...' description='ctrl + s' />
-                    <Dropdown.Item text='Rename' description='ctrl + r' />
-                    <Dropdown.Item text='Make a copy' />
-                    <Dropdown.Item icon='folder' text='Move to folder' />
-                    <Dropdown.Item icon='trash' text='Move to trash' />
-                    <Dropdown.Divider />
-                    <Dropdown.Item text='Download As...' />
-                    <Dropdown.Item text='Publish To Web' />
+                    <Dropdown.Item text='Arts' />
+                    <Dropdown.Item text='Games' />
+                    <Dropdown.Item text='Food & Craft' />
+                    <Dropdown.Item text='Publishing' />
                     <Dropdown.Item text='Comics & Illustration' />
+                    <Dropdown.Item text='Music'  />
+                    <Dropdown.Item text='Design & Tech' />
+                    <Dropdown.Item text='Film' />
                 </Dropdown.Menu>
             </Dropdown>
         )
