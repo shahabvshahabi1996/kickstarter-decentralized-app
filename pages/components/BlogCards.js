c42d8d1c93e489cfb668441385df436f5c70c045
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-22 08:28:15 +0330

    made BlogCard Component dynamic

--- pages/components/BlogCards.js
+++ pages/components/BlogCards.js
@@ -6,69 +6,19 @@ export default class BlogCards extends Component {
         return(
             <Grid stackable columns={3}>
                 <Grid.Row>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
-                            <Card.Content>
-                                <Card.Header>Matthew Harris</Card.Header>
-                                <Card.Meta>Co-Worker</Card.Meta>
-                                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
+                    {this.props.datas.map((data,index)=>{
+                        return(
+                            <Grid.Column>
+                                <a href="#"><Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
+                                    <Card.Content>
+                                        <Card.Header>{data.name}</Card.Header>
+                                        <Card.Meta>Co-Worker</Card.Meta>
+                                        <Card.Description>{data.description.slice(0,30) + '...'}</Card.Description>
+                                    </Card.Content>
+                                </Card></a>
+                            </Grid.Column>
+                        )
+                    })}
                 </Grid.Row>
             </Grid>
         )
