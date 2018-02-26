bbcd36541679b9a34e2437269294fa9719a9c22e
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-26 09:26:47 +0330

    fixing buges for routing and put some links into the head of the html

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,11 +1,11 @@
 import React , {Component} from 'react';
-
+import Head from 'next/head';
 export default class extends Component{
     render(){
         return(
-            <div>
-            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-            </div>
+            <Head>
+                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+            </Head>
         )
     }
 }
