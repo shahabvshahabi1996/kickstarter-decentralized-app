fb5ef83d1efbd7756419842ede43dd591a28bb98
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-28 09:51:19 +0330

    added routing methods from nextjs

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,20 +1,17 @@
 import React , {Component} from "react";
 import { Button, Card, Grid , Divider , Menu , Input , Container , Image , Icon , Popup , Label , Dropdown } from 'semantic-ui-react';
 import Header from './Header';
-
+import Router from 'next/router'
 // import '../styles/App.css';
-
-const options = [
-  { key: 1, text: 'Choice 1', value: 1 },
-  { key: 2, text: 'Choice 2', value: 2 },
-  { key: 3, text: 'Choice 3', value: 3 },
-]
-
 
 export default class Navbar extends Component{
     state = {}
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    handleItemClick = (e, { name }) => {
+      console.log(name);
+      Router.replace(`http://localhost:3000${name}`);
+      this.setState({ activeItem: name });
+    }
   
     render() {
       const { activeItem } = this.state
@@ -22,14 +19,14 @@ export default class Navbar extends Component{
       return (
           <Menu borderless stackable style={{padding : '5px',borderRadius : 0,marginTop:0,borderColor : '#eee',boxShadow: '0px 12px 8px 0px rgba(0,0,0,0.2)',borderWidth : 1,borderStyle : 'solid',padding : '0px'}}>
             <Header/>
-            <Menu.Item>
-              <h2><a href="/" style={{color : '#252525',textDecoration : 'none'}}>KickStarter</a></h2>
+            <Menu.Item name='/' active={activeItem === '/'} onClick={this.handleItemClick}>
+              <h2>KickStarter</h2>
             </Menu.Item>
-            <Menu.Item name='explore' active={activeItem === 'explore'} onClick={this.handleItemClick}>
+            <Menu.Item name='/explore' active={activeItem === '/explore'} onClick={this.handleItemClick}>
             <h4>Explore</h4>
             </Menu.Item>
-            <Menu.Item name='newCampagin' active={activeItem === 'newCampagin'} onClick={this.handleItemClick}>
-              <h4><a href="/campaigns/new" style={{color : '#252525',textDecoration : 'none'}}>Start a campaign</a></h4>
+            <Menu.Item name='/campaigns/new' active={activeItem === '/campaigns/new'} onClick={this.handleItemClick}>
+              <h4>Start a campaign</h4>
             </Menu.Item>
             <Menu.Menu position='right'>
               <Menu.Item>
@@ -39,8 +36,8 @@ export default class Navbar extends Component{
                 content='Look for your dreamy project,and make it alive!'
                 on='focus'/>
               </Menu.Item>
-              <Menu.Item name='help' active={activeItem === 'help'} onClick={this.handleItemClick}>
-                <h4><a href="/login" style={{textDecoration : 'none',color : '#252525'}}>Sign In</a></h4>
+              <Menu.Item name='/login' active={activeItem === '/login'} onClick={this.handleItemClick}>
+                <h4>Sign In</h4>
               </Menu.Item>
             </Menu.Menu>
           </Menu>
