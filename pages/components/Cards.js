63f4d2266c24cd6ad5bec4b553a137a7860ec220
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-28 19:03:19 +0330

    fixing bugses

--- pages/components/Cards.js
+++ pages/components/Cards.js
@@ -4,38 +4,57 @@ import { Button, Card, Grid , Divider , Container , Image , Icon , Popup } from 
 import ProgressBar from './ProgressBar';
 import LikeButton from './LikeButton';
 
+import web3 from '../../web3';
+
 export default class Cards extends Component{
+    
+    constructor(){
+        super();
+        this.state = {
+            datas : undefined
+        }
+    }
+
+    async componentDidMount(){
+        await this.setState({datas : this.props.datas})
+    }
+
     render(){
+        var {datas} = this.state;
+        if(datas != undefined)
         return(
             <Grid stackable>
                 <Grid.Row columns={3}>
-                    {this.props.datas.map((data,index)=>{
+                    {datas.map((data,index)=>{
                         return(
                             <Grid.Column key={index}>
                                 <Card fluid style={{marginTop : '10px',marginBottom : '10px',borderRadius : 1 ,boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)'}}>
                                     <div className="ui fluid image">
-                                        <Image src='https://placeholdit.co//i/580x580?bg=eeeeee' />
-                                        <LikeButton/>
+                                        <Image style={{width : '100%',height : '300px'}} src={data.image} />
+                                        {/* 'https://placeholdit.co//i/580x580?bg=eeeeee' */}
+                                        <LikeButton link={data.campaignAddress}/>
                                     </div>
                                     <Card.Content>
                                         <Card.Header>
-                                            <h4>{data.name.slice(0,50) + '...'}</h4>
+                                            <h4>{data.campaginName === undefined ? ' ' : data.campaginName.length <= 50 ? data.campaginName : data.campaginName.slice(0,50) + '...'}</h4>
                                         </Card.Header>
                                         <Card.Meta>
                                             <h5 className='date Category'>
-                                            Desing & Tech
+                                                {data.category}
                                             </h5>
                                         </Card.Meta>
                                         <Card.Description>
-                                            <p>{data.description.slice(0,50) + '...'}</p>
+                                            <p>{data.aboutCamapaign === undefined ? ' ' : data.aboutCamapaign.length <= 50 ? data.aboutCamapaign : data.aboutCamapaign.slice(0,50) + '...' }</p>
                                         </Card.Description>
                                         <Divider/>
                                         <Card.Description>
-                                            {data.budget}% funded
-                                            <ProgressBar percent={data.budget}/>
+                                            {/* {data.budget}% funded */}
+                                            Pledged of  {web3.utils.fromWei(data.budget,'ether')} ether
+                                            {/* <ProgressBar percent={data.budget}/> */}
                                         </Card.Description>
+                                        <Divider/>
                                         <Card.Description>
-                                        <Button fluid style={{borderRadius : 2,color : '#fff',backgroundColor : '#416DEA',boxShadow: '0px 5px 8px 0px rgba(0,0,0,0.2)'}}>
+                                        <Button onClick={()=>{alert(data.campaignAddress)}} fluid style={{borderRadius : 2,color : '#fff',backgroundColor : '#416DEA',boxShadow: '0px 5px 8px 0px rgba(0,0,0,0.2)'}}>
                                             View Campaign
                                         </Button>
                                         </Card.Description>
@@ -48,5 +67,10 @@ export default class Cards extends Component{
                 </Grid.Row>
             </Grid>
         )
+        else{
+            return(
+                <div></div>
+            )
+        }
     }
 }
