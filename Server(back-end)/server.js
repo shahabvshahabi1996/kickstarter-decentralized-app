44a8856324a7240e7679877d9e93c5d84f08c448
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-13 23:51:24 +0330

    added depencencies of the back-end

--- Server(back-end)/server.js
+++ Server(back-end)/server.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express();
-
+/* --------------------------------------------------------- */
+const ejs = require('ejs');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const cors = require('cors');
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const expressValidator = require('express-validator');
+/* --------------------------------------------------------- */
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser('secret'));
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(flash());
+app.use(expressValidator());
+app.use(express.static(__dirname + './public'));
+/* --------------------------------------------------------- */
 const connection = 'mongodb://<dbuser>:<dbpassword>@ds111319.mlab.com:11319/kickstarter';
-
-const port = 8000;
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/* --------------------------------------------------------- */
+app.set('view engine', 'ejs');
+const routes = require('./routes'); 
+routes(app);
+/* --------------------------------------------------------- */
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
