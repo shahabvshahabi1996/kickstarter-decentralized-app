ebc5f6c2cb63af885754d4348be4e4ed68bd2369
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-15 03:01:38 +0330

    conecting to data base and save a new campaign

--- Server(back-end)/controller/userController.js
+++ Server(back-end)/controller/userController.js
@@ -1,5 +1,49 @@
-exports.addCampagin = (req,res) => {
+const mongoose = require('mongoose');
+var Campaign = require('../model/campaignModel');
 
+
+exports.getAllCampaigns = async (req,res) => {
+    const result = await Campaign.find({});
+    res.json({
+        status : 'success',
+        data : result
+    });
+
+    return;
+}
+
+exports.addCampagin = async (req,res) => {
+    const campaign = await Campaign.findOne({manager : req.body.manager});
+    if(campaign){
+        res.json({ status : 'error' ,message : 'you can only make a single campaign at the moment' })
+        return;
+    }
+    else{
+        console.log(req.body);
+        new Campaign({
+            name : req.body.name,
+            author : req.body.author,
+            category : req.body.category,
+            campaignAddress : req.body.campaignAddress,
+            manager : req.body.manager,
+            info : req.body.info,
+            image : req.body.image,
+            budget : req.body.budget,
+            minimum : req.body.minimum,
+            description : req.body.description
+        }).save(async (err)=>{
+            if(err){
+                res.json({ status : 'error' ,message : err })
+                console.log(err);
+                return;
+            }
+            else{
+                const newCamp = await Campaign.findOne({campaignAddress : req.body.campaignAddress})
+                res.json({ status : 'success',message : 'you posted a new campagin',data : newCamp.campaignAddress});
+                console.log('right')
+            }
+        })
+    }
 }
 
 exports.removeCampagin = (req,res) => {
@@ -23,5 +67,6 @@ exports.sendEmail = (req,res) => {
 }
 
 exports.isValidUser = (req,res) => {
-    
+
 }
+
