3195518cd7ba47b4eeee77d6cf94a3db0b99d7d2
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-14 14:25:27 +0330

    added authController and added commmon methods

---
+++ Server(back-end)/controller/authController.js
@@ -0,0 +1,27 @@
+exports.signup = (req,res) => {
+
+}
+
+exports.login = (req,res,next) => {
+
+}
+
+exports.logout = (req,res) => {
+
+}
+
+exports.deleteAccount = (req,res) => {
+
+}
+
+exports.forgotPassword = (req,res) => {
+
+}
+
+exports.changePassword = (req,res) => {
+
+}
+
+exports.changeEmail = (req,res) => {
+
+}
