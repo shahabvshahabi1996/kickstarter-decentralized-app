fc569b296cafc02a60499703bc3e0eaab04ccb32
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-14 14:27:32 +0330

    added favorite model to the database

---
+++ Server(back-end)/model/favoriteModel.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+const mongodbErrorHandlers = require('mongoose-mongodb-errors');
+const validator = require('validator');
+/*------------------------------------------------------------------------*/
+const connection = 'mongodb://admin:admin@ds111319.mlab.com:11319/kickstarter';
+/*------------------------------------------------------------------------*/
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+/*------------------------------------------------------------------------*/
+
+var favoriteSchema = new Schema({
+    
+    campaignAddress : {
+        type : String,
+        unique : true
+    },
+    user : {
+        type : String,
+        unique : true
+    }
+
+});
+
+/*------------------------------------------------------------------------*/
+mongoose.plugin(mongodbErrorHandlers);
+mongoose.connect(connection);
+/*------------------------------------------------------------------------*/
+module.exports = mongoose.model('Favorite',favoriteSchema );
