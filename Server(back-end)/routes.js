82f92fd586b20fdc3183b27320772d852cb7c06c
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-14 14:24:13 +0330

    added common routes

--- Server(back-end)/routes.js
+++ Server(back-end)/routes.js
@@ -1,9 +1,41 @@
+
+const userController = require('./controller/userController');
+const authController = require('./controller/authController');
+
 module.exports = (app) => {
 
-    app.get('/',(req,res)=>{
-        res.json({
-            message : 'hello world to you shahab'
-        });
-    });
+    app.route('/signup')
+    .post(
+        authController.signup
+    )
 
+    app.route('/logout')
+    .post(
+        authController.logout
+    )
+
+    app.route('/new/campaign')
+    .post(
+        userController.addCampagin
+    )
+
+    app.route('/edit/campaign/:address')
+    .post(
+        userController.editCampagin
+    )
+
+    app.route('/delete/campaign/:address')
+    .post(
+        userController.removeCampagin
+    )
+
+    app.route('/like/campaign/:address')
+    .post(
+        userController.likeCampagin
+    )
+
+    app.route('/report/campagin/:address')
+    .post(
+        userController.reportCampagin
+    )
 }
