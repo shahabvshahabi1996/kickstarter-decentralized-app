8546a1649fd348c715f0c1b8df6cb9fa54d9089c
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-14 14:26:23 +0330

    added userController and common methods

---
+++ Server(back-end)/controller/userController.js
@@ -0,0 +1,27 @@
+exports.addCampagin = (req,res) => {
+
+}
+
+exports.removeCampagin = (req,res) => {
+    
+}
+
+exports.editCampagin = (req,res) => {
+    
+}
+
+exports.likeCampagin = (req,res) => {
+    
+}
+
+exports.reportCampagin = (req,res) => {
+    
+}
+
+exports.sendEmail = (req,res) => {
+
+}
+
+exports.isValidUser = (req,res) => {
+    
+}
