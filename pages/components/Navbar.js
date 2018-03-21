a0f3d3b6944529c14373ca4cce6c9e4d3debaf2f
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-21 07:26:09 +0330

    added methods for authorize the user

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,19 +2,33 @@ import React , {Component} from "react";
 import { Button, Card, Grid , Divider , Menu , Input , Container , Image , Icon , Popup , Label , Dropdown } from 'semantic-ui-react';
 import Header from './Header';
 import {Router} from '../../routes';
+import jwt from 'jsonwebtoken';
 // import '../styles/App.css';
 
 export default class Navbar extends Component{
-    state = {}
+    state = {
+      token : undefined,
+      exp : undefined
+    }
 
     handleItemClick = (e, { name }) => {
       Router.replace(`${name}`);
       this.setState({ activeItem: name });
+    } 
+
+    componentDidMount(){
+      const token = localStorage.getItem('token');
+      if(token){
+        const decoded = jwt.verify(token , 'secretkey');
+        this.setState({ exp : decoded.exp * 1000 , name : decoded.data.name }); 
+      }
+      
+      this.setState({token});
     }
-  
+
     render() {
-      const { activeItem } = this.state
-  
+      const { activeItem , token , name , exp } = this.state;
+      if(token && exp > new Date().getTime())
       return (
           <Menu borderless stackable style={{padding : '5px',borderRadius : 0,marginTop:0,borderColor : '#eee',boxShadow: '0px 12px 8px 0px rgba(0,0,0,0.2)',borderWidth : 1,borderStyle : 'solid',padding : '0px'}}>
             <Header/>
@@ -35,11 +49,37 @@ export default class Navbar extends Component{
                 content='Look for your dreamy project,and make it alive!'
                 on='focus'/>
               </Menu.Item>
+              <Menu.Item name={`/profile/${this.state.token}`} active={activeItem === '/profile'} onClick={this.handleItemClick}>
+                <h4>{name}</h4>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+      )
+
+    else{
+      return(
+        <Menu borderless stackable style={{padding : '5px',borderRadius : 0,marginTop:0,borderColor : '#eee',boxShadow: '0px 12px 8px 0px rgba(0,0,0,0.2)',borderWidth : 1,borderStyle : 'solid',padding : '0px'}}>
+            <Header/>
+            <Menu.Item name='/' active={activeItem === '/'} onClick={this.handleItemClick}>
+              <h2>KickStarter</h2>
+            </Menu.Item>
+            <Menu.Item name='/explore' active={activeItem === '/explore'} onClick={this.handleItemClick}>
+            <h4>Explore</h4>
+            </Menu.Item>
+            <Menu.Menu position='right'>
+              <Menu.Item>
+              <Popup
+                trigger={<Input icon='search' placeholder='Search...' />}
+                header='Campaign Search'
+                content='Look for your dreamy project,and make it alive!'
+                on='focus'/>
+              </Menu.Item>
               <Menu.Item name='/login' active={activeItem === '/login'} onClick={this.handleItemClick}>
                 <h4>Sign In</h4>
               </Menu.Item>
             </Menu.Menu>
           </Menu>
       )
+    }  
     }
 }
