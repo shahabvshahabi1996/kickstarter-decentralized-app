fbfb48532b19a4abd55a5975eb67aff8be089be7
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-03-23 00:07:17 +0330

    added login methods

--- pages/components/LoginForm.js
+++ pages/components/LoginForm.js
@@ -1,7 +1,42 @@
 import React , {Component} from 'react'
 import { Button, Checkbox, Form , Divider } from 'semantic-ui-react'
+import { Router } from '../../routes';
 
 export default class LoginForm extends Component{
+    constructor(){
+        super();
+        this.state = {
+            email : '',
+            password : ''
+        }
+    }
+    async onSubmit(){
+        const { password , email } = this.state;
+        if(password.length > 0 && email.length > 0){
+            const res = await fetch('http://localhost:8000/login', {
+                        method: 'POST',
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            password ,
+                            email
+                        })
+                    });
+    
+            const response = await res.json();
+            if(response.status == 'success'){
+                localStorage.setItem('token',response.token);
+                Router.push('/');
+            }
+            console.log(response);
+        }
+
+        else{
+            alert('plz fill the form validly');
+        }
+    }
     render(){
         return(
             <Form>
@@ -9,14 +44,14 @@ export default class LoginForm extends Component{
                 <hr/>
                 <Form.Field>
                     <label>Email</label>
-                    <input placeholder='john@example.com' />
+                    <input value={this.state.email} name="email" onChange={(event) => {this.setState({[event.target.name] : event.target.value})}} placeholder='john@example.com' />
                 </Form.Field>
                 <Form.Field>
                     <label>Password</label>
-                    <input placeholder='password' />
+                    <input value={this.state.password} name="password" onChange={(event) => {this.setState({[event.target.name] : event.target.value})}} placeholder='password' />
                     <label floated="right"><a href="#">Forgot password ?</a></label>
                 </Form.Field>
-                <Button fluid style={{borderRadius : 2,color : '#fff',backgroundColor : '#416DEA',boxShadow: '0px 5px 8px 0px rgba(0,0,0,0.2)'}}>
+                <Button onClick={this.onSubmit = this.onSubmit.bind(this)} fluid style={{borderRadius : 2,color : '#fff',backgroundColor : '#416DEA',boxShadow: '0px 5px 8px 0px rgba(0,0,0,0.2)'}}>
                     Log in
                 </Button>
                 <Form.Field>
