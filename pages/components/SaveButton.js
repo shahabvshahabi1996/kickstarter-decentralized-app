58e4fb2407a7b2cc8d3e87bf7694410ed6abfca3
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-04-01 21:33:29 +0330

    like buttons fixing

--- pages/components/SaveButton.js
+++ pages/components/SaveButton.js
@@ -3,16 +3,69 @@ import { Icon , Button} from 'semantic-ui-react';
 
 export default class SaveButton extends Component{
     state = {
-        liked : false
+        liked : undefined
     }
+
+    async componentDidMount(){
+        const route = "http://localhost:8000/is/like/campaign/address";
+        const result = await fetch(route,{
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                campaignAddress: this.props.address,
+                token : this.props.token
+            })
+        });
+
+        const res = await result.json();
+        if(res.data){
+            this.setState({liked : true});
+        } else{
+            this.setState({liked : false});
+        }
+    }
+
+    likeAction = async () => {
+        console.log(this.props);
+        const { liked } = this.state; 
+        let route;
+        if(!liked){
+            route = "http://localhost:8000/like/campaign/address"            
+        }
+        else {
+            route = "http://localhost:8000/dislike/campaign/address"
+        }
+        
+        console.log(route);
+
+        const result = await fetch(route,{
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                campaignAddress: this.props.address,
+                token : this.props.token
+            })
+        });
+        
+        const res = await result.json();
+        this.setState({liked : !liked});
+        console.log(res);
+    }
+    
     render(){
         if(!this.state.liked)
         return(
-               <Button onClick={()=>{this.setState({liked : !this.state.liked})}} style={{padding : '12px',fontSize : 15,borderRadius : 1.5}} fluid icon="heart" labelPosition="right" content="Save it!"/>            
+               <Button onClick={this.likeAction} style={{padding : '12px',fontSize : 15,borderRadius : 1.5}} fluid icon="heart" labelPosition="right" content="Save it!"/>            
         )
         else {
             return(
-                <Button onClick={()=>{this.setState({liked : !this.state.liked})}} color="red" style={{padding : '12px',fontSize : 15,borderRadius : 1.5}} fluid content="Saved!"/>                            
+                <Button onClick={this.likeAction} color="red" style={{padding : '12px',fontSize : 15,borderRadius : 1.5}} fluid content="Saved!"/>   
             )
         }
     }
