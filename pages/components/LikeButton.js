dec29b760b6d175a4ed87a97f8992f42ee71c3ce
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-04-01 21:32:30 +0330

    fixing like buttons

--- pages/components/LikeButton.js
+++ pages/components/LikeButton.js
@@ -6,38 +6,86 @@ export default class LikeButton extends Component{
     constructor(){
         super();
         this.state = {
-            active : 0,
-            accounts : []
+            liked : undefined
         }
     }
     async componentDidMount(){
-        let accounts = await web3.eth.getAccounts();
-        this.setState({accounts});
+        const token = await localStorage.getItem('token');
+        if(token){
+            this.setState({token : true});
+        }
+        const route = "http://localhost:8000/is/like/campaign/address";
+        const result = await fetch(route,{
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                campaignAddress: this.props.address,
+                token : token
+            })
+        });
+
+        const res = await result.json();
+        if(res.data){
+            this.setState({liked : true});
+        } else {
+            this.setState({liked : false});
+        }
     }
-    clicked(){
-        this.setState({active : !this.state.active});
+
+    likeAction = async () => {
+        const { liked } = this.state;
+        const token = await localStorage.getItem('token'); 
+        let route;
+
+        if(!liked){
+            route = "http://localhost:8000/like/campaign/address"            
+        }
+        else {
+            route = "http://localhost:8000/dislike/campaign/address"
+        }
+        const result = await fetch(route,{
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                campaignAddress: this.props.address,
+                token : token
+            })
+        });
+        
+        const res = await result.json();
+        this.setState({liked : !liked});
     }
     render(){
-        if(this.state.active){
+        if(this.state.token){
+            if(this.state.liked){
+                return(
+                <div style={{position: 'absolute', top: '3.5%',left : '88%', width: '100%', height: 'auto'}}>
+                    <Popup
+                    trigger={<a onClick={this.likeAction} style={{color : 'red'}}><Icon style={{backgroundColor : 'transparent'}} name='heart' color="red" size='large' /></a>}
+                    content='Saved'
+                    position='top center'
+                    />
+                </div>        
+                )
+            }
+            else
             return(
-            <div style={{position: 'absolute', top: '3.5%',left : '88%', width: '100%', height: 'auto'}}>
-                <Popup
-                trigger={<a onClick={this.clicked = this.clicked.bind(this)} style={{color : 'red'}}><Icon style={{backgroundColor : 'transparent'}} name='heart' color="red" size='large' /></a>}
-                content='Saved'
-                position='top center'
-                />
-            </div>        
+                <div style={{position: 'absolute', top: '3.5%',left : '88%', width: '100%', height: 'auto'}}>
+                    <Popup
+                    trigger={<a onClick={this.likeAction} style={{color : '#252525'}}><Icon style={{backgroundColor : 'transparent'}} name='heart outline' size='large' /></a>}
+                    content='Save it'
+                    position='top center'
+                    />
+                </div>
             )
+        }else{
+            return <div/>
         }
-        else
-        return(
-            <div style={{position: 'absolute', top: '3.5%',left : '88%', width: '100%', height: 'auto'}}>
-                <Popup
-                trigger={<a onClick={this.clicked = this.clicked.bind(this)} style={{color : '#252525'}}><Icon style={{backgroundColor : 'transparent'}} name='heart outline' size='large' /></a>}
-                content='Save it'
-                position='top center'
-                />
-            </div>
-        )
     }
 }
