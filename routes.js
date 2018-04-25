6e5ea0db8a6c3ed5542feeced12bdf163a22349e
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-04-26 01:34:23 +0330

    added profile/show route to the project

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const routes = require('next-routes')();
 
 routes.add('/campaigns/show/:address/:manager','/campaigns/show');
-
+routes.add('/user/profile/show/:id/:token','/user/profile');
 
 module.exports = routes;
