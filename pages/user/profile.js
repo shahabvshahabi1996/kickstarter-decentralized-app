0bbccddc923db12436e8fe62a0f5d40d83aa6782
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-04-27 02:53:39 +0330

    added Content to This Page

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -2,16 +2,38 @@ import React , {Component} from 'react';
 import { Button, Grid , Divider , Container , Advertisement , Dropdown , Tab , Image , Icon , Popup , Label } from 'semantic-ui-react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ProfileContent from '../components/ProfileContent';
 
 export default class Profile extends Component {
+
+    static getInitialProps(props){
+        return { 
+            token : props.query.token
+        };
+    }
+
+    componentDidMount(){
+
+    }
+
     render(){
         return(
             <div style={{backgroundColor:'rgba(65,109,234,1)'}}>    
             <Navbar/>
             <Container style={{padding : '15px',margin: '10px'}}>
-                <div>
-                    <h1>Profile Page</h1>
-                </div>
+                <Grid columns={1} stackable>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <div style={{backgroundColor:'#fff',
+                            padding : '15px',
+                            margin : '5px',
+                            boxShadow: '0px 10px 8px 0px rgba(0,0,0,0.2)',
+                            borderRadius : 1.5}}>
+                                <ProfileContent token={this.props.token}/>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Container>
             <div className="Footer" style={{marginBottom : '10px',borderColor : '#eee',borderStyle:'solid',borderWidth : 1,marginTop : '10px',marginBottom : '0',padding : '15px',backgroundColor : '#fff'}}>
                 <Container>
