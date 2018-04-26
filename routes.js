9d0da5a628a5b961470a2b241374be51e5d57425
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-04-27 02:54:49 +0330

    added a new route and fixing bugses

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const routes = require('next-routes')();
 
 routes.add('/campaigns/show/:address/:manager','/campaigns/show');
-routes.add('/user/profile/show/:id/:token','/user/profile');
+routes.add('/user/profile/show/:token','/user/profile');
+routes.add('/forgot/password','/forgotpassword');
 
 module.exports = routes;
