45dcb4874a05fe3af8e95459f343f9772978cd43
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-05-30 13:40:57 +0330

    adding a new interface and methods to the ui

--- pages/components/ProfileContent.js
+++ pages/components/ProfileContent.js
@@ -1,5 +1,7 @@
 import React , {Component} from 'react';
 import { Image , Card , Grid , Divider } from 'semantic-ui-react';
+import Cards from './Cards';  
+import jwt from 'jsonwebtoken';
 
 
 const About = (
@@ -10,33 +12,81 @@ const About = (
 );
 
 export default class ProfileContent extends Component {
-    
-    componentDidMount = () => {
 
+    constructor() {
+        super();
+        this.state = {
+            campaigns : ''
+        }
+    }
+    
+    componentDidMount = async () => {
+        let fetchUserCamps = await fetch('http://localhost:8000/find/user/campaign',{
+            method : "POST",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body :JSON.stringify({
+                token : this.props.token
+            })
+        });
+
+        let fetchUserLikes = await fetch("http://localhost:8000/find/all/user/likes",{
+            method : "POST",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body :JSON.stringify({
+                token : this.props.token
+            })
+        });
+
+        let likes = await fetchUserLikes.json();
+
+        let campaigns = await fetchUserCamps.json();
+
+        let decoded = jwt.verify(this.props.token , 'secretkey');
+        let name = decoded.data.name;
+        this.setState({campaigns : campaigns.data , name , likes : likes.data});
+        
     }
 
     render(){
+        const { campaigns , name , likes } = this.state;
+        console.log(this.state);
+        if(campaigns && name && likes)
         return(
             <div style={{margin : '5px',marginTop : '15px'}}>
                 <Grid columns={2} stackable>
                     <Grid.Row>
-                        <Grid.Column width={4}>
-                            <Card
-                                image='http://via.placeholder.com/350x350'
-                                header='User Name'
-                                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={12}>
+                        <Grid.Column width={16}>
                             <div>
-                                <h2>User's Campaign</h2>
+                                <h2>{name}'s Campaigns</h2>
                                 <Divider/>
-                                <p>This is where all campaigns renders</p>
+                                <Cards datas={campaigns}/>                             
+                            </div>
+                            <br/>
+                            <div>
+                                {
+                                    likes.length != 0 ?
+                                    <div> 
+                                        <h2>{name}'s Favorites</h2>
+                                        <Divider/>
+                                        <Cards datas={likes}/>
+                                    </div> : 
+                                    <div></div>
+                                }
+                                                             
                             </div>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </div>
         );
+        else {
+            return <div></div>
+        }
     }
 }
