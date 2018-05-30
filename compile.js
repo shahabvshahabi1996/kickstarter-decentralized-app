bf353db05ab9ffacc7e05bb44c91a5663e034e26
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-05-30 13:36:55 +0330

    fixing bugses

--- compile.js
+++ compile.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname,'build');
 fs.removeSync(buildPath);
 
-const contractPath = path.resolve(__dirname,'contracts','Campaign.sol');
+const contractPath = path.resolve(__dirname,'contracts','newCampaign.sol');
 console.log(contractPath);
 const contractFile = fs.readFileSync(contractPath,'utf-8');
 console.log(contractFile);
